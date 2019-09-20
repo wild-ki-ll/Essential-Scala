7fcbd73144846ecf5462628ba0ed1d8320299b96
Kirill Kovalenko <kirill@eldis.ru>
2019-09-20 12:44:31 +0700

    Added lesson 4.2.2.3

---
+++ src/main/scala/chapter4/lesson4.2.2.3.scala
@@ -0,0 +1,20 @@
+package chapter4
+
+object Lesson4_2_2_3 extends App {
+
+  sealed trait DivisionResult;
+  final case class Finite (res: Int) extends DivisionResult;
+  final case object Infinite extends DivisionResult;
+
+  object divide {
+    def apply(a: Int, b: Int): DivisionResult = {
+      b match {
+        case 0 => Infinite;
+        case _ => Finite(a/b);
+      }
+    }
+  }
+
+  println(divide(15, 5) == Finite(3))
+  println(divide(1, 0) == Infinite)
+}
