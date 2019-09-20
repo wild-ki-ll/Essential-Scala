cc6c02fa9d7f7922404c7f33562f7d30abdae7ba
Kirill Kovalenko <kirill@eldis.ru>
2019-09-20 13:33:39 +0700

    Added lesson 4.4.4.2

---
+++ src/main/scala/chapter4/lesson4.4.4.2.scala
@@ -0,0 +1,30 @@
+package chapter4
+
+import chapter4.Lesson4_4_4_2.Fail
+
+object Lesson4_4_4_2 extends App {
+
+  sealed trait Calulator;
+  final case class Succeed(r: Int) extends Calulator;
+  final case class Fail(f: String) extends Calulator;
+
+  sealed trait Oper;
+  final case object + extends Oper;
+  final case object - extends Oper;
+  final case object / extends Oper;
+  final case object * extends Oper;
+
+  def calc (a: Int, o: Oper, b: Int): Calulator =
+    o match {
+      case + => Succeed(a + b);
+      case - => Succeed(a - b);
+      case / => if (b == 0) Fail("Division by zero") else Succeed(a/b);
+      case * => Succeed(a * b)
+    }
+
+  println(calc(1, +, 0) == Succeed(1))
+  println(calc(1, -, 0) == Succeed(1))
+  println(calc(1, /, 1) == Succeed(1))
+  println(calc(1, *, 1) == Succeed(1))
+  println(calc(1, /, 0) == Fail("Division by zero"))
+}
