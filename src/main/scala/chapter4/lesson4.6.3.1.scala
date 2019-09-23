448440f6f2c8f72a0b2275ba7c135beec634d290
Kirill Kovalenko <kirill@eldis.ru>
2019-09-23 15:24:46 +0700

    Fixed lesson 4.6.3.1

--- src/main/scala/chapter4/lesson4.6.3.1.scala
+++ src/main/scala/chapter4/lesson4.6.3.1.scala
@@ -7,13 +7,28 @@ object Lesson4_6_3_1 extends App {
   def lengthTailRec (acc: Int, lst: IntList): Int = {
     lst match {
       case End => acc;
-      case Pair(h, t) => lengthTailRec(acc+1, t)
+      case Pair(_, t) => lengthTailRec(acc+1, t)
+    }
+  }
+  @tailrec
+  def propductTailRec (acc: Int, lst: IntList): Int = {
+    lst match {
+      case End => acc;
+      case Pair(h, t) => propductTailRec(acc * h, t)
     }
   }
 
   sealed trait IntList {
     def length: Int = lengthTailRec(0, this)
+    def product: Int = propductTailRec(1, this)
+    def double: IntList =
+      this match {
+        case End => End;
+        case Pair(h, t) => Pair(h * 2, t.double)
+      }
+
   }
+
   case object End extends IntList
   final case class Pair(head: Int, tail: IntList) extends IntList
 
@@ -21,4 +36,13 @@ object Lesson4_6_3_1 extends App {
   println(example.length == 3)
   println(example.tail.length == 2)
   println(End.length == 0)
+
+  println(example.product == 6)
+  println(example.tail.product == 6)
+  println(End.product == 1)
+
+  println(example.double == Pair(2, Pair(4, Pair(6, End))))
+  println(example.tail.double == Pair(4, Pair(6, End)))
+  println(End.double == End)
+
 }
