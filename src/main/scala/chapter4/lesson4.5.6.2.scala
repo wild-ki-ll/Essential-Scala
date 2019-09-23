2e9b24d4d671bee1e53195d6759811b43a32e335
Kirill Kovalenko <kirill@eldis.ru>
2019-09-23 15:46:10 +0700

    Fixed lesson 4.5.6.2

--- src/main/scala/chapter4/lesson4.5.6.2.scala
+++ src/main/scala/chapter4/lesson4.5.6.2.scala
@@ -18,9 +18,24 @@ object Lesson4_5_6_2 extends App {
         case Failure(r) => Failure(r)
       }
     }
+    def / (calc: Calculation, oper: Int): Calculation = {
+      calc match {
+        case Success(i) => {
+          oper match {
+            case 0 => Failure("Division by zero")
+            case _ => Success(i / oper)
+          }
+        }
+        case Failure(r) => Failure(r)
+      }
+    }
   }
 
   println(Calculator.+(Success(1), 1) == Success(2))
   println(Calculator.-(Success(1), 1) == Success(0))
   println(Calculator.+(Failure("Badness"), 1) == Failure("Badness"))
+
+  println(Calculator.+(Success(1), 1) == Success(2))
+  println(Calculator./(Success(4), 0) == Failure("Division by zero"))
+  println(Calculator./(Failure("Badness"), 0) == Failure("Badness"))
 }
