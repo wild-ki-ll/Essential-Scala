0d622db1270951d07cf40b442d534c87a4f1195c
Kirill Kovalenko <k.b.kovalenko@gmail.com>
2019-10-09 20:24:02 +0700

    Added lesson 5.1.3.3

---
+++ src/main/scala/chapter5/lesson5.1.3.3.scala
@@ -0,0 +1,18 @@
+package chapter5
+
+object Lesson5_1_3_3 extends App {
+  sealed trait LinkedList[A] {
+    def contains (i: A): Boolean =
+      this match {
+        case Pair(h, t) => if (h == i) true else t.contains(i)
+        case End() => false
+      }
+  }
+  final case class End[A] () extends LinkedList[A]
+  final case class Pair[A](head: A, tail: LinkedList[A]) extends LinkedList[A]
+
+  val example = Pair(1, Pair(2, Pair(3, End())))
+  println(example.contains(3) == true)
+  println(example.contains(4) == false)
+  println(End().contains(0) == false)
+}
